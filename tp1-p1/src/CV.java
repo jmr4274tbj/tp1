91292372976b9f026725a64d856105343ceb932a
Jonathan Martel-Raiche <jonathanmr@sympatico.ca>
2019-01-31 12:00:01 -0500

    Ajout constructeur classe CV

--- tp1-p1/src/CV.java
+++ tp1-p1/src/CV.java
@@ -5,7 +5,17 @@ public class CV {
 	String prenom;
 	String formation;
 	int nbAnneesExperience;
-	String[] competences;
+	String competences;
 	String attentes4B4;
+	
+	public CV(String nom, String prenom, String formation, 
+		int nbAnneesExperience, String competences, String attentes) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.nbAnneesExperience = nbAnneesExperience;
+		this.competences = competences;
+		this.attentes4B4 = attentes4B4;
+	}
 	
 }
