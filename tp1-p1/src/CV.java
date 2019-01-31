26746e54a7985ae4c2d33ab2e633409e08b6bc03
Jonathan Martel-Raiche <jonathanmr@sympatico.ca>
2019-01-31 12:10:37 -0500

    Ajout methode afffiche

--- tp1-p1/src/CV.java
+++ tp1-p1/src/CV.java
@@ -1,7 +1,7 @@
 
 public class CV {
 	
-	String nom;
+    String nom;
 	String prenom;
 	String formation;
 	int nbAnneesExperience;
@@ -9,7 +9,7 @@ public class CV {
 	String attentes4B4;
 	
 	public CV(String nom, String prenom, String formation, 
-		int nbAnneesExperience, String competences, String attentes) {
+		int nbAnneesExperience, String competences, String attentes4B4) {
 		this.nom = nom;
 		this.prenom = prenom;
 		this.formation = formation;
@@ -18,4 +18,12 @@ public class CV {
 		this.attentes4B4 = attentes4B4;
 	}
 	
+	public void affiche() {		
+		System.out.println("\nNom : " + nom);	
+		System.out.println("\nPrénom : " + prenom);		
+		System.out.println("\nFormation(s) : " + formation);		
+		System.out.println("\nNombre d'années d'expérience" + nbAnneesExperience);		
+		System.out.println("\nCompétences : " + competences);	
+		System.out.println("\nAttentes vis à vis le cours 4B4 : " + attentes4B4);	
+	}
 }
