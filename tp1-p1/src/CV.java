26c9db30cf3382cb9a9c9a4d4cfe967c50a43106
Jonathan Martel-Raiche <jonathanmr@sympatico.ca>
2019-01-31 12:11:59 -0500

    Ajout methode main

--- tp1-p1/src/CV.java
+++ tp1-p1/src/CV.java
@@ -18,6 +18,18 @@ public class CV {
 		this.attentes4B4 = attentes4B4;
 	}
 	
+	public static void main(String[] args) {		
+		System.out.println("Bienvenue chez Barette!");
+		
+		CV cv1 = new CV("Taleb", "Rayane", "Informatique, plus particulièrement en programmation", 
+				2, "Java", "Que le cours soit intéréssant.");
+		cv1.affiche();
+		
+		CV cv2 = new CV("Martel-Raiche", "Jonathan", "Informatique, principalement programmation", 
+				2, "Java, Javascript Jquery, html, css, C#", "Que les travaux soient surtout fait en classe.");
+		cv2.affiche();	
+	}
+	
 	public void affiche() {		
 		System.out.println("\nNom : " + nom);	
 		System.out.println("\nPrénom : " + prenom);		
